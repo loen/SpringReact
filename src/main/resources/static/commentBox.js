a358ff71ce4d677499f72145140e6fd07b7823f9
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2016-12-31 18:52:29 +0100

    load component lists from javascript tables

--- src/main/resources/static/commentBox.js
+++ src/main/resources/static/commentBox.js
@@ -1,12 +1,34 @@
 class CommentBox extends React.Component {
+
+    _getComments(){
+        const commentsList = [
+            {id: 1, author:"Billy Jean", body:"It's not my love" },
+            {id: 2, author:"Bender", body:"Beer baby"}
+        ]
+
+        return commentsList.map((comment) =>{
+           return(<Comment author={comment.author} body={comment.body} key={comment.id} />);
+        });
+    }
+
+    _getCommentsTitle(commentsCount){
+        if(commentsCount === 0){
+            return "No comments yet!";
+        }else if(commentsCount === 1){
+            return "1 comment";
+        } else {
+            return `${commentsCount} comments`;
+        }
+    }
+
     render(){
+        const comments = this._getComments();
         return(
             <div className="comment-box">
                 <h3>Comments</h3>
-                <h4 className="comment-count">2 comments</h4>
+                <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
                 <div className="comment-list">
-                    <Comment author="Mc Callan" body="Scottish comment yeah !" />
-                    <Comment author="Bender" body="hell yeah boby"/>
+                    {comments}
                 </div>
             </div>
         );
