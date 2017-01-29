f081efe4ce7b05b0cadda80f0921f01a2135e193
Andrzej Pozlutko <andrzej.pozlutko@gmail.com>
2017-01-29 15:00:09 +0100

    add groups test

--- src/main/java/com/andrzej/pe/security/groups/Groups.java
+++ src/main/java/com/andrzej/pe/security/groups/Groups.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Groups {
 
+    public static final String STORMPATH_AUTHORIZED_ADMIN_GROUP_HREF = "stormpath.authorized.admin.group.href";
     public final String ADMIN;
 
     @Autowired
     public Groups(Environment env) {
-        this.ADMIN = env.getProperty("stormpath.authorized.admin.group.href");
+        this.ADMIN = env.getProperty(STORMPATH_AUTHORIZED_ADMIN_GROUP_HREF);
     }
 }
